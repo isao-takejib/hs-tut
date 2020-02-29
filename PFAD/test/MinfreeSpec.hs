b25af4350666a7d8377c8a5f15ab062c1b1cba25
isao-takejib <isao.takeji.bigmoon@gmail.com>
2020-02-29 07:27:55 +0000

    Add naive qc property

--- PFAD/test/MinfreeSpec.hs
+++ PFAD/test/MinfreeSpec.hs
@@ -2,6 +2,9 @@ module MinfreeSpec (spec) where
 
 import Test.Hspec
 import Minfree
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck
+
 
 spec :: Spec
 spec = do
@@ -12,3 +15,9 @@ spec = do
   describe "minfree'" $
     it "書籍の実行例" $
       minfree' [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] `shouldBe` 15
+
+  describe "minfree == minfree'" $
+    prop "minfree == minfree'" prop_Minfree
+
+prop_Minfree :: [Int] -> Bool
+prop_Minfree xs = minfree xs == minfree' xs
