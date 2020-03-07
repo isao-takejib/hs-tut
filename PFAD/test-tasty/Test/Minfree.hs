7d5b606fbebced2783223d3de7dc3f70e8c8cfad
isao-takejib <isao.takeji.bigmoon@gmail.com>
2020-03-07 05:40:02 +0000

    Add tasty-quickcheck

--- PFAD/test-tasty/Test/Minfree.hs
+++ PFAD/test-tasty/Test/Minfree.hs
@@ -4,9 +4,18 @@ import Minfree
 
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+
+import Data.List (nub)
 
 unit_case1 :: IO ()
 unit_case1 = minfree [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] @?= 15
 
 unit_case2 :: IO ()
 unit_case2 = minfree' [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] @?= 15
+
+prop_Minfree :: [Positive Int] -> Property
+prop_Minfree xs = preCondition ==> minfree ns  == minfree' ns
+  where
+      ns = map getPositive xs
+      preCondition = length (nub xs) == length xs
