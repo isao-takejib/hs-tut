30278aa26c83a8b39ef2679c675e79c08a0cf847
isao-takejib <isao.takeji.bigmoon@gmail.com>
2020-03-07 05:23:41 +0000

    Add tasty example

--- PFAD/test-tasty/Test/Minfree.hs
+++ PFAD/test-tasty/Test/Minfree.hs
@@ -1,5 +1,12 @@
-module Test.Minfree () where
+module Test.Minfree where
 
 import Minfree
 
 import Test.Tasty
+import Test.Tasty.HUnit
+
+unit_case1 :: IO ()
+unit_case1 = minfree [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] @?= 15
+
+unit_case2 :: IO ()
+unit_case2 = minfree' [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] @?= 15
