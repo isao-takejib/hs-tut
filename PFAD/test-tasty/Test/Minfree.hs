ec84ef571d8b22521af473f7bae253cd9bd26198
isao-takejib <isao.takeji.bigmoon@gmail.com>
2020-03-07 05:52:14 +0000

    Add tasty-hspec example

--- PFAD/test-tasty/Test/Minfree.hs
+++ PFAD/test-tasty/Test/Minfree.hs
@@ -5,6 +5,8 @@ import Minfree
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
+import Test.Tasty.Hspec
+import Test.Hspec.QuickCheck (prop)
 
 import Data.List (nub)
 
@@ -19,3 +21,16 @@ prop_Minfree xs = preCondition ==> minfree ns  == minfree' ns
   where
       ns = map getPositive xs
       preCondition = length (nub xs) == length xs
+
+spec_hspec :: Spec
+spec_hspec = do
+  describe "minfree" $
+    it "書籍の実行例" $
+      minfree [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] `shouldBe` 15
+
+  describe "minfree'" $
+    it "書籍の実行例" $
+      minfree' [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] `shouldBe` 15
+
+  describe "minfree == minfree'" $
+    prop "minfree == minfree'" prop_Minfree
